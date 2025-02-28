72580754bc1c40367a90a5f170e94d93d51885a5
Markic <marko1998.marac@gmail.com>
2025-02-28 17:15:01 +0100

    My-list

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const itemForm=document.querySelector("#item-form");
 const itemInput=document.querySelector("#item-input");
 const ul=document.querySelector("#item-list");
+const clearBtn=document.querySelector("#clear");
 
 function run(e){
     e.preventDefault();
@@ -23,4 +24,22 @@ function createButton(ids){
  return button;
 }
 
+function removeItem(e){
+    if(e.target.parentElement){if (confirm("Are you sure?"));
+{e.target.parentElement.remove()}
+    
+}}
+
+ 
+
+function clearItems(){
+   
+    while (ul.firstChild){
+        ul.removeChild(ul.firstChild);
+    }
+}
+
 itemForm.addEventListener("submit", run);
+
+clearBtn.addEventListener("click", clearItems);
+ul.addEventListener("click", removeItem);
